c0f2b512a055c667cb751ef4526ea744f2428826
Steven G. Johnson <stevenj@mit.edu>
2014-07-15 15:32:53 -0400

    rm ruby and pgsql plugins: keep libutf8proc repo focused exclusively on the C library

--- pgsql/utf8proc.sql
+++
@@ -1,6 +0,0 @@
-CREATE OR REPLACE FUNCTION unifold (text) RETURNS text
-  LANGUAGE 'c' IMMUTABLE STRICT AS '$libdir/utf8proc_pgsql.so',
-  'utf8proc_pgsql_unifold';
-CREATE OR REPLACE FUNCTION unistrip (text) RETURNS text
-  LANGUAGE 'c' IMMUTABLE STRICT AS '$libdir/utf8proc_pgsql.so',
-  'utf8proc_pgsql_unistrip';
--- ruby/extconf.rb
+++
@@ -1,2 +0,0 @@
-require 'mkmf'
-create_makefile("utf8proc_native")
