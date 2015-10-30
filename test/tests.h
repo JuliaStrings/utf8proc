6acc41dfe91099bc858d7b0ebc8d27be37477318
Peter Colberg <peter@colberg.org>
2015-10-30 15:22:09 -0400

    Fix implicit function declarations

--- test/tests.h
+++ test/tests.h
@@ -1,5 +1,13 @@
 /* Common functions and includes for our test programs. */
 
+/*
+ * Set feature macro to enable getline() and wcwidth().
+ *
+ * Please refer to section 2.2.1 of POSIX.1-2008:
+ * http://pubs.opengroup.org/onlinepubs/9699919799/functions/V2_chap02.html#tag_15_02_01_02
+ */
+#define _XOPEN_SOURCE 700
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
