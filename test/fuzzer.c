d61dc5cbad299231cb94b9fd024064359c2dfd3f
Randy <randy408@protonmail.com>
2021-01-23 16:38:05 +0100

    add fuzz target

---
+++ test/fuzzer.c
@@ -0,0 +1,17 @@
+#include <utf8proc.h>
+
+#ifdef __cplusplus
+extern "C"
+#endif
+int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    if(size < 1) return 0;
+
+    free(utf8proc_NFD(data));
+    free(utf8proc_NFC(data));
+    free(utf8proc_NFKD(data));
+    free(utf8proc_NFKC(data));
+    free(utf8proc_NFKC_Casefold(data));
+
+    return 0;
+}
