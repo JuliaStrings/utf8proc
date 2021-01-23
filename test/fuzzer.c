7c065a4c4094350d46237693b4683cd0cfb2a3d2
Randy <randy408@protonmail.com>
2021-01-23 17:25:40 +0100

    update fuzzer

--- test/fuzzer.c
+++ test/fuzzer.c
@@ -7,6 +7,8 @@ int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
 {
     if(size < 1) return 0;
 
+    if(data[size-1] != '\0') return 0;
+
     free(utf8proc_NFD(data));
     free(utf8proc_NFC(data));
     free(utf8proc_NFKD(data));
