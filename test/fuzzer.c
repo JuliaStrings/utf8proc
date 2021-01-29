c17ea5dfefcad1085b1b0cea9456709c8c3611a8
Randy <randy408@protonmail.com>
2021-01-29 19:54:58 +0100

    OSS-Fuzz initial integration (#216)
    * add fuzz target
    
    * update fuzzer
    
    * add fuzzer to build with basic entry point
    
    * add build script
    
    * cleanup
    
    * build fuzz target using cmake in oss-fuzz env
    
    * ossfuzz.sh add newline
    
    * update build

---
+++ test/fuzzer.c
@@ -0,0 +1,16 @@
+#include <utf8proc.h>
+
+int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size)
+{
+    if(size < 1) return 0;
+
+    if(data[size-1] != '\0') return 0;
+
+    free(utf8proc_NFD(data));
+    free(utf8proc_NFC(data));
+    free(utf8proc_NFKD(data));
+    free(utf8proc_NFKC(data));
+    free(utf8proc_NFKC_Casefold(data));
+
+    return 0;
+}
