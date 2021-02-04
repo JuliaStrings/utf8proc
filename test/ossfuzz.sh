8349a8797dca62ead42d5f65f949ed9bcb3a91e2
Randy <randy408@protonmail.com>
2021-02-04 16:24:14 +0100

    fix build

--- test/ossfuzz.sh
+++ test/ossfuzz.sh
@@ -4,10 +4,13 @@
 
 mkdir build
 cd build
-cmake .. -DUTF8PROC_ENABLE_TESTING=ON -DLIB_FUZZING_ENGINE="$LIB_FUZZING_ENGINE"
+cmake ..
 make -j$(nproc)
 
-cp $SRC/utf8proc/build/fuzzer utf8proc_fuzzer
+$CC $CFLAGS -I$SRC/utf8proc \
+    $SRC/utf8proc/test/fuzzer.c \
+    -o $OUT/utf8proc_fuzzer \
+    $LIB_FUZZING_ENGINE $SRC/utf8proc/build/libutf8proc.a
 
 find $SRC/utf8proc/test -name "*.txt" | \
      xargs zip $OUT/utf8proc_fuzzer_seed_corpus.zip
