005526e961a05df72a334b2bc485bf99edb2179a
Erik Schnetter <schnetter@gmail.com>
2025-06-11 13:40:44 -0400

    Correct build errors

---
+++ data/download.sh
@@ -0,0 +1,7 @@
+wget https://www.unicode.org/Public/17.0.0/ucd/CaseFolding.txt
+wget https://www.unicode.org/Public/17.0.0/ucd/CompositionExclusions.txt
+wget https://www.unicode.org/Public/17.0.0/ucd/DerivedCoreProperties.txt
+wget https://www.unicode.org/Public/17.0.0/ucd/EastAsianWidth.txt
+wget https://www.unicode.org/Public/17.0.0/ucd/UnicodeData.txt
+wget https://www.unicode.org/Public/17.0.0/ucd/auxiliary/GraphemeBreakProperty.txt
+wget https://www.unicode.org/Public/17.0.0/ucd/emoji/emoji-data.txt
