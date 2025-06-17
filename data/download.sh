c28fc22acdf8a028b2d2f2c89a733b6bb6c32e64
Erik Schnetter <schnetter@gmail.com>
2025-06-17 14:30:47 -0400

    Remove unwanted file

--- data/download.sh
+++
@@ -1,7 +0,0 @@
-wget https://www.unicode.org/Public/17.0.0/ucd/CaseFolding.txt
-wget https://www.unicode.org/Public/17.0.0/ucd/CompositionExclusions.txt
-wget https://www.unicode.org/Public/17.0.0/ucd/DerivedCoreProperties.txt
-wget https://www.unicode.org/Public/17.0.0/ucd/EastAsianWidth.txt
-wget https://www.unicode.org/Public/17.0.0/ucd/UnicodeData.txt
-wget https://www.unicode.org/Public/17.0.0/ucd/auxiliary/GraphemeBreakProperty.txt
-wget https://www.unicode.org/Public/17.0.0/ucd/emoji/emoji-data.txt
