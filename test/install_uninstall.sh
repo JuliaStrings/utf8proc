30ebb28572f9a34b7d7264f733fdb7ae0468bddb
tab <tab@0x09.net>
2026-01-28 17:17:34 -0500

    Remove install test
    This was based on the contents of the generated MANIFEST, but this
    is generated by running `make install` itself, so this test was
    circuitous.

--- test/install_uninstall.sh
+++ test/install_uninstall.sh
@@ -6,20 +6,11 @@ if [ -z "$install_dir" ]; then
   exit 1
 fi
 
-# test installation
 if ! make install DESTDIR="$install_dir" prefix=""; then
 	echo "FAILED: make install" >&2
 	rm -rf "$install_dir"
 	exit 1
 fi
-
-cut -d ' ' -f 1 MANIFEST.new | while read -r installed_file; do
-	if [ ! -e "$install_dir/$installed_file" ]; then
-		echo "FAILED: make install of "$installed_file"" >&2
-		rm -rf "$install_dir"
-		exit 1
-	fi
-done
 
 # test uninstallation
 if ! make uninstall DESTDIR="$install_dir" prefix=""; then
